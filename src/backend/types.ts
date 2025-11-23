07790063bbd95f399929dedb8a98d5404b2ecc5d
Talha Agro <talha.agro50@gmail.com>
2025-11-23 14:06:04 -0500

    define schema for all the different IO types we can have

---
+++ src/backend/types.ts
@@ -0,0 +1,27 @@
+// this is the structure of each song track that will display on the frontend
+export interface Song {
+    title: string;
+    artist: string;
+    year: string;
+    link: string;
+    genre: string;
+}
+
+//this is the structure of each final output after analysis that will display on the frontend
+export interface AnalysisResult{
+    recognised: Song;
+    recommendations: Song[];
+    message: string;
+}
+
+//this is just for convenience, for each credited developer
+export interface DevProfile {
+    name: string;
+    role: string;
+    imageURL: string;
+    email: string;
+}
+
+//Hard coded values for the moods and genres list
+export const MOODS = ['Sad', 'Nostalgic', 'Happy', 'Energetic', 'Chill', 'Focus', 'Romantic', 'Angry'];
+export const GENRES = ['Jazz', 'Pop', 'Rock', 'Classical', 'Hip-Hop', 'Electronic', 'R&B', 'Country', 'Indie'];
